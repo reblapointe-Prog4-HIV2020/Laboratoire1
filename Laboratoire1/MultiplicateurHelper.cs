72cf994f239c076a032e4b94d9a16080a1cdb23e
RebeccaLapointe <RebeccaLapointe@INFO-324-1A-122>
2020-01-20 12:15:28 -0500

    Les arrays contenant les multiplicateurs et leur valeur dans Multiplicateur Helper sont maintenant statiques, readonly et leur contenu est immuable.

--- Laboratoire1/MultiplicateurHelper.cs
+++ Laboratoire1/MultiplicateurHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,14 @@ namespace CegepJonquiere.RebLapointe.Laboratoire1
 {
     public class MultiplicateurHelper
     {
-        private static readonly string[] MULTIPLICATEURS = { "n", "µ", "m", "", "k", "M", "G" };
-        private static readonly double[] MULTVAL = { 0.000_000_001, 0.000_001, 0.001, 1, 1_000, 1_000_000, 1_000_000_000 };
+        private static readonly IReadOnlyList<string> MULTIPLICATEURS = new [] { "n", "µ", "m", "", "k", "M", "G" };
+        private static readonly IReadOnlyList<double> MULTVAL = new[] { 0.000_000_001, 0.000_001, 0.001, 1, 1_000, 1_000_000, 1_000_000_000 };
 
         private const double PRECISION = 10.0; // 1/PRECISION est la véritable précision
 
         public static string SymboleMult(double valeur)
         {
-            for (int i = MULTIPLICATEURS.Length - 1; i >= 0; i--)
+            for (int i = MULTIPLICATEURS.Count - 1; i >= 0; i--)
                 if (valeur >= MULTVAL[i])
                     return MULTIPLICATEURS[i];
             return "";
@@ -24,7 +25,7 @@ namespace CegepJonquiere.RebLapointe.Laboratoire1
 
         public static double Multiplicateur(double valeur)
         {
-            for (int i = MULTIPLICATEURS.Length - 1; i >= 0; i--)
+            for (int i = MULTIPLICATEURS.Count - 1; i >= 0; i--)
                 if (valeur >= MULTVAL[i])
                     return MULTVAL[i];
             return 1;
